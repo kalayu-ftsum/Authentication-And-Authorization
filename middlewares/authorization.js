a6902b2e21853a794cdd3f45596d13720692740e
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2023-10-30 13:02:37 +0100

    Implemented attribute based access control

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const RBACAuthorization=(role)=>{
-  return function(req, res, next) {
+  return (req, res, next)=> {
     const token =  req.headers.authorization.split(' ')[1];
     if (token) {
       jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
@@ -23,6 +23,30 @@ const RBACAuthorization=(role)=>{
 };
 
 
+
+function ABACAuthorization(attributes) {
+    return (req, res, next)=> {
+        const token =  req.headers.authorization.split(' ')[1];
+        if (token) {
+          jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+              // handle error
+              res.status(403).json({msg:'Invalid token'});
+            } else if (!attributes.every(attr => user[attr])) {
+                res.status(403).json({msg:'Invalid token'});
+            } else {
+              req.user = user;
+              next();
+            }
+          });
+        } else {
+          res.status(403).json({msg:'No token provided'});
+        }
+      };
+}
+
+
 module.exports={
-    RBACAuthorization:RBACAuthorization
+    RBACAuthorization,
+    ABACAuthorization
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const { passport } = require('../utils/auth');
-const {RBACAuthorization}= require('../middlewares/authorization');
+const {RBACAuthorization, ABACAuthorization}= require('../middlewares/authorization');
 
 router.get('/protected',passport.authenticate('jwt', { session: false }), RBACAuthorization("admin"),
     (req, res) => {
@@ -13,8 +13,19 @@ router.get('/protected',passport.authenticate('jwt', { session: false }), RBACAu
             return res.status(500).send('Internal Server Error.');
         }
         }
-    )
+);
 
+router.get('/allowed',passport.authenticate('jwt', { session: false }), ABACAuthorization(["role"]),
+    (req, res) => {
+        try {
+            if(!req.user) return res.status(401).send('Unauthorized')
+            const { username, email } = req.user;
+            res.status(200).send({ user: { username, email } });
+        } catch (error) {
+            return res.status(500).send('Internal Server Error.');
+        }
+        }
+);
 
 
 
